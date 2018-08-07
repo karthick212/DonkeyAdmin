7248e338ce6d82a1f571808e6abddd0a7ab5f31b
Karthick <vbk005@hotmail.com>
2018-08-07 22:09:59 +0530

    dff

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-//import AdminRoutes from './admin'
-import CourierRoutes from './courier'
+import AdminRoutes from './admin'
+//import CourierRoutes from './courier'
 
 Vue.use(Router)
 
 const baseRoutes = [
 ];
 
-const routes=baseRoutes.concat(CourierRoutes);
+const routes=baseRoutes.concat(AdminRoutes);
 
 export default new Router({
 routes,
